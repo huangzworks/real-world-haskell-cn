7451a2982b2e037fae3123be66c32baf36623557
huangz1990 <huangz1990@gmail.com>
2012-09-07 18:11:27 +0800

    完成第三章《类型别名》小节

--- code/ch03/BookStore.hs
+++ code/ch03/BookStore.hs
@@ -9,3 +9,17 @@ data MagzineInfo = Magzine Int String [String]
 -- file: ch03/BookStore.hs
 myInfo = Book 9780135072455 "Algebra of Programming"
               ["Richard Bird", "Oege de Moor"]
+
+-- file: ch03/BookStore.hs
+-- 稍后就会介绍 CustomerID 的定义
+
+data BookReview = BookReview BookInfo CustomerID String
+
+-- file: ch03/BookStore.hs
+type CustomerID = Int
+type ReviewBody = String
+
+data BetterReview = BetterReview BookInfo CustomerID ReviewBody
+
+-- file: ch03/BookStore.hs
+type BookRecord = (BookInfo, BookReview)
