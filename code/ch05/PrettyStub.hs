dd6f5d631121d48c1adaea7d3af29e026a389f6a
huangz1990 <huangz1990@gmail.com>
2012-09-12 22:43:28 +0800

    完成第五章《Haskell 开发诀窍》小节

---
+++ code/ch05/PrettyStub.hs
@@ -0,0 +1,14 @@
+-- file: ch05/PrettyStub.hs
+import SimpleJSON
+
+data Doc = ToBeDefined
+         deriving (Show)
+
+string :: String -> Doc
+string str = undefined
+
+text :: String -> Doc
+text str = undefined
+
+double :: Double -> Doc
+double num = undefined
