32104528c42833fae5d055f676bc6e956396fcfc
huangz1990 <huangz1990@gmail.com>
2012-09-12 10:05:24 +0800

    完成第五章《在 Haskell 中表示 JSON 数据》小节

---
+++ code/ch05/SimpleJSON.hs
@@ -0,0 +1,27 @@
+-- file: ch05/SimpleJSON.hs
+
+data JValue = JString String
+            | JNumber Double
+            | JBool Bool
+            | JNull
+            | JObject [(String, JValue)]
+            | JArray [JValue]
+              deriving (Eq, Ord, Show)
+
+getString :: JValue -> Maybe String
+getString (JString s) = Just s
+getString _           = Nothing
+
+getInt (JNumber n) = Just (truncate n)
+getInt _           = Nothing
+
+getBool (JBool b) = Just b
+getBool _         = Nothing
+
+getObject (JObject o) = Just o
+getObject _           = Nothing
+
+getArray (JArray a) = Just a
+getArray _          = Nothing
+
+isNull v            = v == JNull
