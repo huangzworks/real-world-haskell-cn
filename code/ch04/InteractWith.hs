733eca3d33981b330bef7146c259b0324fe77007
huangz1990 <huangz1990@gmail.com>
2013-05-18 20:27:32 +0800

    修复第四章关于 InteractWith.hs 源码的错误，感谢 @kosl90 的提醒

--- code/ch04/InteractWith.hs
+++ code/ch04/InteractWith.hs
@@ -3,15 +3,15 @@
 import System.Environment (getArgs)
 
 interactWith function inputFile outputFile = do
-    input <- readFile inputFile
-    writeFile outputFile (function input)
+  input <- readFile inputFile
+  writeFile outputFile (function input)
 
 main = mainWith myFunction
   where mainWith function = do
-        args <- getArgs
-        case args of
-            [input, output] -> interactWith function input output
+          args <- getArgs
+          case args of
+            [input,output] -> interactWith function input output
             _ -> putStrLn "error: exactly two arguments needed"
 
---replace "id" with the name of our function below
-myFunction = id
+        -- replace "id" with the name of our function below
+        myFunction = id
