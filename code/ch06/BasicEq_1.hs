95171b8a1385d609df47330276a173a5e09f0e6f
sancao2 <linjiangxiandong@163.com>
2015-12-07 02:26:28 +0800

    几乎重新翻译"Important Built-In Typeclasses"前面的章节.
    交代一下为什么要重新翻译一遍这几段,又为什么文件名字改回成eqclasses,而不是BasicEq_{1|2|3}.
    1. 原译文缺少BasicEq_2.hs, 一下子出现BasicEq_3.hs,所以我才查看英文原文.
    我发现,原来英文作者这里的命名更加合理.
    如果将eqclasses的拆成三个文件BasicEq_{1|2|3}.hs,而里面的类型类的方法都叫isEqual,
    那么下面出现的isEqual就不知道指代英文里面的isEqual{1|2|3}哪个了,至少我就产生歧义了,233.
    2. 我觉得原译文不够生动有趣,太教条了,居高临下,而英文作者的文字其实很生动活泼,特别是他的人称代词都是"we",而很少"you".
    而原译文基本采用人称代词"you",感觉像命令的口气或者指导的口气,有些句子缺乏主语,给人的感觉生硬.

--- code/ch06/BasicEq_1.hs
+++
@@ -1,9 +0,0 @@
--- file: ch06/BasicEq_1.hs
-
-class BasicEq a where
-    isEqual :: a -> a -> Bool
-
-instance BasicEq Bool where
-    isEqual True  True  = True
-    isEqual False False = True
-    isEqual _     _     = False
---
+++ code/ch06/naiveeq.hs
@@ -0,0 +1,19 @@
+-- file: ch06/naiveeq.hs
+data Color = Red | Green | Blue
+
+colorEq :: Color -> Color -> Bool
+colorEq Red   Red   = True
+colorEq Green Green = True
+colorEq Blue  Blue  = True
+colorEq _     _     = False
+
+stringEq :: [Char] -> [Char] -> Bool
+
+-- Match if both are empty
+stringEq [] [] = True
+
+-- If both start with the same char, check the rest
+stringEq (x:xs) (y:ys) = x == y && stringEq xs ys
+
+-- Everything else doesn't match
+stringEq _ _ = False
