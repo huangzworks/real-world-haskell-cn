e038d5b4ed02ab07b02b810241fe4fa2bcf7053e
sancao2 <linjiangxiandong@163.com>
2015-12-07 14:32:58 +0800

    添加翻译了Read的部分.
    有两书缺少的:1.read.hs的例子以及其中根据程序中字面值进行类型推导的说明,2.最后的readsPrec的例子.

--- code/ch06/naiveeq.hs
+++ code/ch06/naiveeq.hs
@@ -1,4 +1,5 @@
 -- file: ch06/naiveeq.hs
+import Data.Char (isSpace)
 data Color = Red | Green | Blue
 
 colorEq :: Color -> Color -> Bool
@@ -27,3 +28,33 @@ instance Show Color where
     show Red   = "Color 1: Red"
     show Green = "Color 2: Green"
     show Blue  = "Color 3: Blue"
+
+-- instance Read Color where
+--     -- readsPrec is the main function for parsing input
+--     readsPrec _ value =
+--         -- We pass tryParse a list of pairs.  Each pair has a string
+--         -- and the desired return value.  tryParse will try to match
+--         -- the input to one of these strings.
+--         tryParse [("Red", Red), ("Green", Green), ("Blue", Blue)]
+--         where tryParse [] = []    -- If there is nothing left to try, fai
+--               tryParse ((attempt, result):xs) =
+--                     -- Compare the start of the string to be parsed to the
+--                     -- text we are looking for.
+--                   if (take (length attempt) value) == attempt
+--                   -- If we have a match, return the result and the
+--                   -- remaining input
+--                   then [(result, drop (length attempt) value)]
+--                   -- If we don't have a match, try the next pair
+--                   -- in the list of attempts.
+--                   else tryParse xs
+
+instance Read Color where
+    readsPrec _ value = tryParse colors
+        where
+          cleanedUpValue = dropWhile isSpace value
+          tryParse [] = []
+          tryParse ((attempt, result):xs) =
+              if (take (length attempt) cleanedUpValue) == attempt
+              then [(result, drop (length attempt) cleanedUpValue)]
+              else tryParse xs
+          colors = [("Red", Red), ("Green", Green), ("Blue", Blue)]
