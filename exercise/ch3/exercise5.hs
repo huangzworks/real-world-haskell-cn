6c8f0f17cdec5d213321d71b0847ea65c6e159e9
Hong <31026481+whong5@users.noreply.github.com>
2018-03-11 11:04:38 +1100

    Update exercise5.hs
    String like "stats" should also be palindrome

--- exercise/ch3/exercise5.hs
+++ exercise/ch3/exercise5.hs
@@ -1,5 +1,6 @@
 isPalindrome :: Eq a => [a] -> Bool
-isPalindrome [] = True
+isPalindrome []  = True
+isPalindrome [a] = True
 isPalindrome (x:xs)
     | x == (last xs) = isPalindrome (init xs)
     | otherwise = False
